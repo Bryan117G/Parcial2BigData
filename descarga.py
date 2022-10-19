8293775f0b68042f7cb4fc7baf232fe3fc28ff7c
Bryan117G <97194493+Bryan117G@users.noreply.github.com>
2022-10-18 21:08:43 -0500

    Update descarga.py

--- descarga.py
+++ descarga.py
@@ -16,36 +16,6 @@ import time
 from bs4 import BeautifulSoup
 import ast
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
-
-# Script generated for node S3 bucket
-S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={"paths": ["s3://parcialbryangaravito"], "recurse": True},
-    transformation_ctx="S3bucket_node1",
-)
-
-# Script generated for node ApplyMapping
-ApplyMapping_node2 = ApplyMapping.apply(
-    frame=S3bucket_node1, mappings=[], transformation_ctx="ApplyMapping_node2"
-)
-
-# Script generated for node S3 bucket
-S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=ApplyMapping_node2,
-    connection_type="s3",
-    format="json",
-    connection_options={"path": "s3://parcialbryangaravito", "partitionKeys": []},
-    transformation_ctx="S3bucket_node3",
-)
-
 s3 = boto3.client('s3')
 ahora = datetime.now()
 papers = ['eltiempo','elespectador']
@@ -59,6 +29,6 @@ for i in papers:
     meses = ['enero','febrero','marzo','abril','mayo','junio']
     ruta = 'news/raw/periodico={}/year={}/month={}/day={}/{}.txt'.format(i,ahora.year,meses[ahora.month-1],ahora.day,name)
     s3.upload_file("/tmp/doc.txt","parcialbryangaravito",ruta)
-    s3.upload_file("/tmp/doc.txt","parcialbryangaravito","headlines/raw/{}.txt".format(i))
+    s3.upload_file("/tmp/doc.txt","parcialbryangaravito","news/raw/{}.txt".format(i))
 
 job.commit()
